566eb6a1f9793bd8b108ce15366eca018b746283
ibrahimahmed443 <ibrahimahmed443@gmail.com>
2015-10-15 16:56:33 +0500

    change text

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from app import hello
 class TestCases(unittest.TestCase):
 
 	def test_hello(self):
-		self.assertEqual(hello(), 'Hello World!')
+		self.assertEqual(hello(), 'Hello Jenkins!')
 
 if __name__ == '__main__':
     unittest.main()
