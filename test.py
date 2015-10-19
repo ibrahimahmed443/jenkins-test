078cb37340688469e2067ec13dcbd218167b268c
ibrahimahmed443 <ibrahimahmed443@gmail.com>
2015-10-19 14:31:39 +0500

    change text in test file

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from app import hello
 class TestCases(unittest.TestCase):
 
 	def test_hello(self):
-		self.assertEqual(hello(), 'Hello Everyone!')
+		self.assertEqual(hello(), 'Hello Jenkins!')
 
 if __name__ == '__main__':
     unittest.main()
